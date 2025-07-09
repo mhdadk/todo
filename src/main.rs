4c400f6313d15097c0d3d5c99ed14368bf0ade43
Mahmoud Abdelkhalek <mabdelkh@mathworks.com>
2025-07-09 16:30:56 -0400

    self.modify

--- src/main.rs
+++ src/main.rs
@@ -41,10 +41,10 @@ impl TodoApp {
     }
     fn add(&self) -> Result<(), String> {
         let h = io::stdin();
-        println!("Let's create a new task. First, the description: ");
+        print!("Let's create a new task. First, the description: ");
         let mut description = String::new();
         h.read_line(description).expect("Cannot read input: ");
-        println!("Next, the date and time the task is due: ");
+        print!("Next, the date and time the task is due (in RFC3339 format): ");
         let mut due_datetime = String::new();
         h.read_line(due_datetime).expect("Cannot read input: ");
         let due_datetime = DateTime::parse_from_rfc3339(due_datetime.as_str())
@@ -59,8 +59,48 @@ impl TodoApp {
         self.tasks.push(task);
         Ok(())
     }
-    fn modify(&self, task_id: i32, task_details: Task) -> Result<(), String> {}
-    fn mark_as_completed(&mut self, id: i32) -> Result<(), &str> {
+    fn modify(&self) -> Result<(), String> {
+        print!("Enter id for the task that you would like to modify: ")
+        let mut input = String::new();
+        io::stdin().read_line(&mut input);
+        let id: i32 = input.parse().unwrap();
+        // check that task with this id exists
+        let task_exists = false;
+        for t in &mut self.tasks {
+            if t.id == id {
+                let task = t;
+                task_exists = true;
+            }
+        }
+        if !task_exists {
+            return Err("Could not find task.");
+        }
+        print!("Enter new task description (leave empty if unchanged): ")
+        input.clear()
+        io::stdin().read_line(&mut input);
+        if input != "" {
+            task.description = input;
+        }
+        print!("Enter new task due date and time in RFC3339 format (leave empty if unchanged): ")
+        input.clear()
+        io::stdin().read_line(&mut input);
+        if input != "" {
+            let due_datetime = DateTime::parse_from_rfc3339(input.as_str())
+            .unwrap()
+            .with_timezone(&Utc);
+            task.due_datetime = due_datetime;
+        }
+        Ok(())
+    }
+    fn mark_as_completed(&mut self) -> Result<(), &str> {
+        // get task id from user
+        print!("Enter id for the task that you would like to mark as completed: ")
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Input could not be read.");
+        let id: i32 = input.parse().unwrap();
+        // can do binary search here since the ids are sorted
         for task in &mut self.tasks {
             if task.id == id {
                 task.completed = true;
@@ -69,7 +109,9 @@ impl TodoApp {
         }
         Err("Could not find task.")
     }
-    fn delete(&self, task_id: i32) -> Result<(), String> {}
+    fn delete(&self) -> Result<(), String> {
+        // TODO
+    }
     fn display(&self) -> () {}
     fn run(self) -> ! {
         loop {
@@ -89,6 +131,8 @@ impl TodoApp {
             match opt {
                 1 => self.add(),
                 2 => self.mark_as_completed(),
+                3 => self.modify(),
+                4 => self.delete(),
                 _ => Ok(()),
             };
         }
